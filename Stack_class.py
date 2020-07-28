1c2884d1b19c5c0bd8df2a32799999ee36f97122
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-07-28 12:50:15 +0300

    final commit

--- Stack_class.py
+++ Stack_class.py
@@ -1,130 +1,53 @@
 
-
-
-
-
 class Stack:
-    stack =[]
-    def __init__(self, *numbers):
-        for number in numbers:
-            Stack.stack.append(number)
+
+    def __init__(self):
+        self.stacklist = []
 
     def isEmpty(self):
         '''проверка стека на пустоту. Метод возвращает True или False.'''
-        if len(Stack.stack) > 0:
-            print(f'Стэк не пустой')
-            print(Stack.stack) # Как вывести содержимое?
-            return False
-        else:
-            print('Стэк пустой')
-            return True
+        return self.stacklist == []
 
-    def push (self, *elements):
+    def push (self, element):
         '''добавляет новый элемент на вершину стека. Метод ничего не возвращает.'''
-        Stack.stack.extend(elements)
-        print(Stack.stack)
+        self.stacklist.append(element)
 
     def pop(self):
         ''' удаляет верхний элемент стека. Стек изменяется. Метод возвращает верхний элемент стека'''
-        pop_elem = Stack.stack.pop()
-        print(f'Удален верхний элемент стэка {pop_elem}')
-        return pop_elem
+        if not self.isEmpty():
+            return self.stacklist.pop()
 
     def peek(self):
         '''возвращает верхний элемент стека, но не удаляет его. Стек не меняется.'''
-        print('Верхний элемент стека: ', Stack.stack[-1])
-        return Stack.stack[-1]
+        if not self.isEmpty():
+            return self.stacklist[len(self.stacklist)-1]
 
     def size(self):
         '''возвращает количество элементов в стеке'''
-        print('Количество элементов в стеке: ', len(Stack.stack))
-        return len(Stack.stack)
-#
-MySampleStack = Stack()
-MySampleStack.isEmpty()
+        return len(self.stacklist)
 
-MySampleStack = Stack(6, [1231, 23423, 234], 10, {2234: {2342: {234234: [12312, 41321, 1241]}}},15,-5)
-MySampleStack.isEmpty()
-MySampleStack.push(7, 15, {1000: {12312: [123, 123, 123]}})
-MySampleStack.pop()
-MySampleStack.peek()
-MySampleStack.size()
-
-'''TASK 2'''
-
-# def check_string_balance(string):
-#     pass
-round_pair = {'(': ')'}
-square_pair = {'[': ']'}
-figure_pair = {'{': '}'}
-pairs = {'(': ')', '[': ']', '{': '}', ')': '(', ']': '[', '}': '{'}
-'''
-Условия:
-элементов д.б. четное кол-во
-кол-во открывающих элементов д.б равно кол-ву закрывающих
-Как учесть порядок элеменов????????
-'''
-string = '(((([{}]))))'
-ext_string = '(((([{)]})))'
-# string = '[([])((([[[]]])))]{()}'
-
-print(string)
-reversed_string = list(reversed(string))
-print(reversed_string)
-if len(string) % 2 == 0:
-    print('Проверку на четность скобок прошли')
-else:
-    print('Проверку на четность скобок НЕ прошли')
-
-if string.count('(') == string.count(')') and \
-    string.count('[') == string.count(']') and \
-    string.count('{') == string.count('}'):
-    print('Проверку на одинаковое кол-во открывашек и закрывашек прошли')
-else:
-    print('Проверку на одинаковое кол-во открывашек и закрывашек НЕ прошли')
-
-i=0
-for symbol in string:
-    if pairs[string[i]] == reversed_string[i]:
-        # print(pairs[string[i]], '==> OK', i, reversed_string[i])
-        i+=1
+def check_string_balance(string):
+    pairs = {')': '(', ']': '[', '}': '{'}
+    brackets = ['{', '}', '[', ']', '(', ')']
+    my_sample_stack = Stack()
+    for symbol in string:
+        if symbol in brackets:
+            if symbol in pairs.values():
+                my_sample_stack.push(symbol)
+            else:
+                if pairs[symbol] != my_sample_stack.peek():
+                    return 'Строка несбалансирована'
+                else:
+                    my_sample_stack.pop()
+    if my_sample_stack.isEmpty():
+        return "Строка сбалансирована"
     else:
-        print('Проверку на соответствие откр/закр скобок НЕ прошли')
-        break
-    print('Проверяю на соответствие откр/закр скобок')
+        return "Строка несбаланрирована"
 
 
-'''
-
-'''
-
-
-# print(round_pair[string[0]])
-# i = 0
-# j =-1
-# for symbol in string:
-#     if round_pair[string[i]] == string[j]:
-#         print(round_pair[string[i]],'OK', string[j])
-#         i+=1
-#         j-=1
-#     else:
-#         if square_pair[string[i]] == string[j]:
-#             print(square_pair[string[i]],'OK', string[j])
-#             i+=1
-#             j-=1
-#         else:
-#             if figure_pair[string[i]] == string[j]:
-#                 print(figure_pair[string[i]],'OK', string[j])
-#                 i+=1
-#                 j-=1
-
-
-
-
-
-
-
-# else:
-#     print('Строка несбалансирована')
-# [([])((([[[]]])))]{()}
-# ')(', '][', '}{', ']{', '){', '}['
+print(check_string_balance('[([])((([[[]]])))]{()}'))
+print(check_string_balance('(((([{}]))))'))
+print()
+print(check_string_balance('}{}'))
+print(check_string_balance('{{[(])]}}'))
+print(check_string_balance('[[{())}]'))
